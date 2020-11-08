8461362b47faec6e1b5ea5e55cde63d8bccb8ba6
riferrei <riferrei@riferrei.com>
2020-11-08 14:49:17 -0500

    Added support for audit/packet/metric beat on frontend

--- frontend-react/entrypoint.sh
+++ frontend-react/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+metricbeat setup && service metricbeat start
+packetbeat setup && service packetbeat start
 heartbeat setup && service heartbeat-elastic start
+auditbeat setup && service auditbeat start
 
 exec npm run-script start
